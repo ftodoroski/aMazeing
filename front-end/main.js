4a8504c4c26390b7d25dc013433234ec87e457ec
Granit <ghaxhaj@fordham.edu>
2020-03-03 16:54:35 -0500

    hi

--- front-end/main.js
+++ front-end/main.js
@@ -1,8 +1,11 @@
+
 var cols, rows; //these variables are the columns and rows 
 var s = 25 // this is the s of a sell 
 var grid = []
 var current;
 var stack = []
+var next;
+
 
 
 
@@ -10,13 +13,14 @@ function setup(){ // this function sets up the canvas
     createCanvas(400,400)
     cols = floor(width/s)
     rows = floor(height/s)
-    // frameRate(1000)
+    frameRate(100000)
 
     
 
     for (var j = 0; j < rows; j++){
         for(var i = 0; i < cols; i++){
-            var cell = new Cell(i,j)
+            var cell
+            cell = new Cell(i,j)
             grid.push(cell)
         } // ends second for 
     } // ends first for
@@ -33,7 +37,7 @@ function draw(){
     current.visited = true
     current.highlight()
 
-    var next = current.checkNeighbors()
+    next = current.checkNeighbors(current.i, current.j)
     if(next){
         next.visited = true
         
@@ -45,10 +49,10 @@ function draw(){
 
     } else if(stack.length > 0){
         current = stack.pop()
-
     }
 
 } // ends draw function
+
 
 function index(i,j){ // needed to make the program think of the array as one dimmensional
     if(i < 0 || j < 0 || i > cols -1 || j > rows - 1){
@@ -57,19 +61,31 @@ function index(i,j){ // needed to make the program think of the array as one dim
     return i + j * cols 
 } // ends index function
 
-function Cell(i,j){ // this function holds the cell position, column and row
+function Cell(i,j){ // this is a constructor function for a cell object 
     this.i = i
     this.j = j
     this.walls = [true,true,true,true]
     this.visited = false
 
-    this.checkNeighbors = function(){
+    this.checkNeighbors = function(i, j){
+        // console.log("made it ")
+        // console.log(i, j)
+
+        
         var neighbors = [];
 
         var top = grid[index(i,j-1)];
         var right = grid[index(i+1,j)];
         var bottom = grid[index(i,j+1)];
         var left = grid[index(i-1,j)];
+
+        // console.log("top", top)
+        // console.log("right", right)
+        // console.log("bottom", bottom)
+        // console.log("left", left)
+
+
+
 
         if(top && !top.visited){
             neighbors.push(top)
@@ -87,22 +103,23 @@ function Cell(i,j){ // this function holds the cell position, column and row
         if(neighbors.length > 0 ){
             var r = floor(random(0, neighbors.length))
             return neighbors[r]
-        } else {
-            return undefined;
-        }
+        } 
     } // ends checkneighbor function
+
     this.highlight = function(){
         var x = this.i*s;
         var y = this.j*s;
         noStroke()
-        fill(0,0.222,100)
+        fill(100,100.0,0)
         rect(x,y,s,s)
     }
 
     this.show = function(){
         var x = this.i*s;
         var y = this.j*s;
-        stroke(255);
+        stroke(100);
+        
+        
         if (this.walls[0]){
         line(x,y,x+s,y)}
 
@@ -119,13 +136,15 @@ function Cell(i,j){ // this function holds the cell position, column and row
         
         if(this.visited){
         noStroke()
-        fill(255, 0, 255, 100); //simple color coordinates
+        fill(400, 100, 255, 100); //simple color coordinates
         rect(x,y,s,s);
         }
     } // ends show function
 } // ends Cells function    
 
 function removeWalls(a,b){
+    // console.log("this is A", a)
+    // console.log("this is B", b)
     var x = a.i - b.i 
     if(x === 1){
         a.walls[3] = false
@@ -145,5 +164,43 @@ function removeWalls(a,b){
         b.walls[0] = false  
     }
 }// ends removewalls
+document.addEventListener("keydown", function(e){
+     
+    if(e.keyCode === 39){ // right 
+        var rightNeighbor = grid[index(current.i+1,current.j)]
+        if(rightNeighbor.walls[3]==false){
+    current.i += 1}
+    } else if(e.keyCode === 37){
+        var leftNeighbor = grid[index(current.i-1,current.j)];
+        if(leftNeighbor.walls[1]==false)
+    current.i -= 1
+    }else if(e.keyCode === 38){
+        var bottomNeighbor = grid[index(current.i,current.j-1)];
+        if(bottomNeighbor.walls[2]==false)
+    current.j -= 1
+   
+    }else if(e.keyCode === 40){
+        var bottomNeighbor = grid[index(current.i,current.j+1)];
+        if(bottomNeighbor.walls[0]==false)
+    current.j += 1
+ 
+
+    }
+
+    // if(e.keyCode === 39) {  
+    //     console.log(current.walls)
+    //     current.i += 1
+    // }
+})
+
+
+function endOfGame(current){
+    if(current.i === 15 && current.j === 15){
+    // fetch()
+    }
+}
+
+
+
 
 
--- front-end/move.js
+++ front-end/move.js
@@ -9,28 +9,28 @@ function anim(e){
 if(e.keyCode == 39){ // right
     guyLeft += 10
     guy.style.left = guyLeft + 'px'
-    if(guyLeft >=4710){
-        guyLeft -=10
-    }
+    // if(guyLeft >=4710){
+    //     guyLeft -=10
+    // }
 
 } else if(e.keyCode == 37){
     guyLeft -= 10    
     guy.style.left = guyLeft + 'px'
-    if(guyLeft <=0){
-        guyLeft +=10
-    }
+    // if(guyLeft <=0){
+    //     guyLeft +=10
+    // }
 
 
 }else if(e.keyCode == 38){
     guyDown -= 10    
     guy.style.top = guyDown + 'px'
-    if(guyDown <=0){
-        guyDown +=10}
+    // if(guyDown <=0){
+    //     guyDown +=10}
     }else if(e.keyCode == 40){
     guyDown += 10    
     guy.style.top = guyDown + 'px'
-    if(guyDown >=4710){
-        guyDown -=10}
+    // if(guyDown >=4710){
+    //     guyDown -=10}
     }
 
 }
